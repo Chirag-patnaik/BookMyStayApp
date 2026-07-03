e7cb185c4f026d7ac4aad3911d794ada8acac8d0
chirag-patnaik <chiragpatnaik788@gmail.com>
2026-07-03 13:39:39 +0530

    uc1: Application entry and welcome message

---
+++ BookMyStayApp.java
@@ -0,0 +1,27 @@
+/**
+ * Use Case 1
+ * ------------------------------
+ * Book My Stay App
+ * Hotel Booking Management System
+ *
+ * Demonstrates the application entry point
+ * and welcome message.
+ *
+ * @author Chirag Patnaik
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println("          BOOK MY STAY APP");
+        System.out.println("========================================");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version : 1.0");
+        System.out.println();
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Application Started Successfully.");
+    }
+}
